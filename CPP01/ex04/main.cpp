b5851120bf0b1c4d373de3e31f4dc58587fc3264
mahmoudskhairi <mahmoud.skhairi@gmail.com>
2025-01-13 22:25:25 +0100

    some modification in CPP01/ex04

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -3,28 +3,30 @@
 
 int main(int ac, char *av[])
 {
-    std::ifstream SourceFile(av[1]);
-    std::ofstream DestinationFile("dest.replace");
     std::string s1 = av[2];
     std::string s2 = av[3];
+    std::string dest_file = av[1];
+    std::ifstream SourceFile(dest_file.c_str());
+    dest_file.append(".replace");
+    std::ofstream DestinationFile(dest_file.c_str());
     std::string line;
-    ssize_t index = 0;
     if (ac == 4)
     {
         if (SourceFile.is_open() && SourceFile.is_open())
         {
             while (std::getline(SourceFile, line))
             {
-                if (s1.compare(s2))
+                ssize_t index = line.find(s1, 0);
+                while (index != -1)
                 {
-                    while (index != -1)
+                    if (index == -1) // std::string::npos == -1 means that s1 is not found!!
                     {
-                        index = line.find(s1);
-                        if (index == -1) // std::string::npos == -1 means that s1 is not found!!
-                            break;
-                        line.erase(index, s1.length());
-                        line.insert(index, s2);
+                        index = 0;
+                        break;
                     }
+                    line.erase(index, s1.length());
+                    line.insert(index, s2);
+                    index = line.find(s1, index + s2.length());
                 }
                 DestinationFile << line + "\n";
             }
