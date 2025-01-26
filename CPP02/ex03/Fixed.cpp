b9ecb9e8058fdf7bbb4afc67a3a4f65cc0acc0a7
mahmoudskhairi <mahmoud.skhairi@gmail.com>
2025-01-26 20:17:10 +0100

    CPP02/ex03 folder (BSP) was created

---
+++ CPP02/ex03/Fixed.cpp
@@ -0,0 +1,151 @@
+#include "Fixed.hpp"
+
+int Fixed::_FractionnalBits = 8;
+/* Default constructor*/
+Fixed::Fixed() : _FixedPoint(0)
+{
+}
+
+/* Parameterized constructors*/
+Fixed::Fixed(const int FixedValue) : _FixedPoint(FixedValue << _FractionnalBits)
+{
+}
+
+Fixed::Fixed(const float FloatValue) : _FixedPoint(roundf(FloatValue * (1 << _FractionnalBits)))
+{
+}
+
+/* Copy Constructor*/
+
+Fixed::Fixed(const Fixed &CopiedFixed)
+{
+    this->setRawBits(CopiedFixed.getRawBits());
+}
+
+/* conversion methods */
+float Fixed::toFloat(void) const
+{
+    // std::cout << "*" << (int)(1 << this->_FractionnalBits) << std::endl;
+    return ((float)this->_FixedPoint / (1 << this->_FractionnalBits));
+}
+
+int Fixed::toInt(void) const
+{
+    return (this->_FixedPoint);
+}
+
+/* setters & getters */
+int Fixed::getRawBits(void) const
+{
+    return (this->_FixedPoint);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    this->_FixedPoint = raw;
+}
+
+/* Destructor */
+Fixed::~Fixed()
+{
+}
+
+/* Arithmetic Operators*/
+Fixed &Fixed::operator=(const Fixed &fixed)
+{
+    if (this != &fixed)
+        this->setRawBits(fixed.getRawBits());
+    return (*this);
+}
+Fixed Fixed::operator+(const Fixed &fixed)
+{
+    return (Fixed(this->toFloat() + fixed.toFloat()));
+}
+Fixed Fixed::operator-(const Fixed &fixed)
+{
+    return (Fixed(this->toFloat() - fixed.toFloat()));
+}
+Fixed Fixed::operator*(const Fixed &fixed)
+{
+    return (Fixed(this->toFloat() * fixed.toFloat()));
+}
+
+Fixed Fixed::operator/(const Fixed &fixed)
+{
+    return (Fixed(this->toFloat() / fixed.toFloat()));
+}
+
+/* Comparison Operators*/
+bool Fixed::operator>(const Fixed ComparedFixed)
+{
+    return (this->getRawBits() > ComparedFixed.getRawBits());
+}
+bool Fixed::operator<(const Fixed ComparedFixed)
+{
+    return (this->getRawBits() < ComparedFixed.getRawBits());
+}
+bool Fixed::operator>=(const Fixed ComparedFixed)
+{
+    return (this->getRawBits() >= ComparedFixed.getRawBits());
+}
+bool Fixed::operator<=(const Fixed ComparedFixed)
+{
+    return (this->getRawBits() <= ComparedFixed.getRawBits());
+}
+bool Fixed::operator==(const Fixed ComparedFixed)
+{
+    return (this->getRawBits() == ComparedFixed.getRawBits());
+}
+bool Fixed::operator!=(const Fixed ComparedFixed)
+{
+    return (this->getRawBits() != ComparedFixed.getRawBits());
+}
+
+/* prefix and postfix increment */
+Fixed &Fixed::operator++(void)
+{
+    ++this->_FixedPoint;
+    return (*this);
+}
+Fixed Fixed::operator++(int)
+{
+    Fixed tmp(*this);
+    this->_FixedPoint++;
+    return (tmp);
+}
+
+/* prefix and postfix dicrement */
+Fixed &Fixed::operator--(void)
+{
+    --this->_FixedPoint;
+    return (*this);
+}
+
+Fixed Fixed::operator--(int)
+{
+    Fixed tmp(*this);
+    this->_FixedPoint--;
+    return (tmp);
+}
+
+/* max and min methods*/
+const Fixed &Fixed::min(Fixed &a, const Fixed &b)
+{
+    if (a.getRawBits() < b.getRawBits())
+        return (a);
+    return (b);
+}
+
+const Fixed &Fixed::max(Fixed &a, const Fixed &b)
+{
+    if (a.getRawBits() > b.getRawBits())
+        return (a);
+    return (b);
+}
+
+/* Insertion Operator */
+std::ostream &operator<<(std::ostream &OutStrem, const Fixed &Fixed)
+{
+    OutStrem << Fixed.toFloat();
+    return (OutStrem);
+}
---
+++ CPP02/ex03/Fixed.hpp
@@ -0,0 +1,57 @@
+#ifndef _FIXED_HPP_
+#define _FIXED_HPP_
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+private:
+    int _FixedPoint;
+    static int _FractionnalBits;
+
+public:
+    /* default constructor*/
+    Fixed();
+
+    /* parameterized constructors*/
+    Fixed(const int FixedValue);
+    Fixed(const float FloatValue);
+
+    /* copy constructor*/
+    Fixed(const Fixed &CopiedFixed);
+
+    /* convesion methods*/
+    float toFloat(void) const;
+    int toInt(void) const;
+
+    /* getters & setters*/
+    int getRawBits(void) const;
+    void setRawBits(int const raw);
+
+    /* comparison operators */
+    bool operator>(const Fixed ComparedFixed);
+    bool operator<(const Fixed ComparedFixed);
+    bool operator>=(const Fixed ComparedFixed);
+    bool operator<=(const Fixed ComparedFixed);
+    bool operator==(const Fixed ComparedFixed);
+    bool operator!=(const Fixed ComparedFixed);
+    /* arithmetic operators */
+    Fixed &operator=(Fixed const &fixed);
+    Fixed operator+(Fixed const &fixed);
+    Fixed operator-(Fixed const &fixed);
+    Fixed operator*(Fixed const &fixed);
+    Fixed operator/(Fixed const &fixed);
+    /* increment and decrement operators*/
+    Fixed &operator++(void);
+    Fixed &operator--(void);
+    Fixed operator++(int);
+    Fixed operator--(int);
+    /* min and max methods*/
+    static const Fixed &min(Fixed &a, const Fixed &b);
+    static const Fixed &max(Fixed &a, const Fixed &b);
+
+    ~Fixed();
+};
+/* insertion operator */
+std::ostream &operator<<(std::ostream &OutStraem, const Fixed &fixed);
+#endif
