776ac8543ce92af3057e8da6ada74eafdf86cd15
mahmoudskhairi <mahmoud.skhairi@gmail.com>
2025-02-03 14:21:23 +0100

    CPP03/ex00 was finished

--- CPP03/ex00/ClapTrap.hpp
+++ CPP03/ex00/ClapTrap.hpp
@@ -12,13 +12,22 @@ private:
     unsigned int _DamagePoints;
 
 public:
-    ClapTrap();
     ClapTrap(std::string Name);
-    ClapTrap(ClapTrap target);
+    ClapTrap(ClapTrap &target);
     ~ClapTrap();
+    ClapTrap &operator=(ClapTrap &clap);
     void attack(const std::string &target);
     void takeDamage(unsigned int amount);
     void beRepaired(unsigned int amount);
+    /* getters and setters */
+    void SetName(std::string Name);
+    std::string GetName();
+    void SetHealth(unsigned int Health);
+    unsigned int GetHealth();
+    void SetEnergie(unsigned int Energie);
+    unsigned int GetEnergie();
+    void SetDamage(unsigned int Damage);
+    unsigned int GetDamage();
 };
 
 #endif
--- CPP03/ex00/main.cpp
+++ CPP03/ex00/main.cpp
@@ -2,7 +2,45 @@
 
 int main()
 {
-    ClapTrap target1;
-    ClapTrap target2;
-    // ClapTrap target1;
+    std::cout << "\n----------------------------------------------------------------------" << std::endl;
+    ClapTrap clap1("clap1");
+    std::cout << "----------------------------------------------------------------------" << std::endl;
+
+    for (int i = 0; i < 6; i++)
+    {
+        clap1.attack("zombie");
+        std::cout << "#clap1 Health points: " << clap1.GetHealth() << std::endl;
+        std::cout << "#clap1 Energy points: " << clap1.GetEnergie() << std::endl;
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        clap1.takeDamage(3);
+        std::cout << "#clap1 Health points: " << clap1.GetHealth() << std::endl;
+        std::cout << "#clap1 Energy points: " << clap1.GetEnergie() << std::endl;
+    }
+    std::cout << "----------------------------------------------------------------------" << std::endl;
+    ClapTrap clap2("clap2");
+    std::cout << "----------------------------------------------------------------------" << std::endl;
+    for (int i = 0; i < 4; i++)
+    {
+        clap2.attack("zombie");
+        std::cout << "#clap2 Health points: " << clap2.GetHealth() << std::endl;
+        std::cout << "#clap2 Energy points: " << clap2.GetEnergie() << std::endl;
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        clap2.takeDamage(1);
+        std::cout << "#clap2 Health points: " << clap2.GetHealth() << std::endl;
+        std::cout << "#clap2 Energy points: " << clap2.GetEnergie() << std::endl;
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        clap2.beRepaired(3);
+        std::cout << "#clap2 Health points: " << clap2.GetHealth() << std::endl;
+        std::cout << "#clap2 Energy points: " << clap2.GetEnergie() << std::endl;
+    }
+    clap2.takeDamage(20);
+    // clap2.attack("zombie");
+    std::cout << "#clap2 Health points: " << clap2.GetHealth() << std::endl;
+    std::cout << "#clap2 Energy points: " << clap2.GetEnergie() << std::endl;
 }
