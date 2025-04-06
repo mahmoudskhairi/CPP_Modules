ec68b2b5434465d3e512503c71a4a44b23ee8541
mahmoudskhairi <mahmoud.skhairi@gmail.com>
2025-04-06 20:51:50 +0100

    template specialisation test

--- testing/templates/templateSpecialisation.cpp
+++ testing/templates/templateSpecialisation.cpp
@@ -16,10 +16,23 @@ public:
     ~MyClass() {};
 };
 
+template <>
+class MyClass<std::string>
+{
+private:
+    std::string _data;
+
+public:
+    MyClass() {};
+    MyClass(std::string data) : _data(data) {};
+    std::string Getdata() { return this->_data; }
+    ~MyClass() {};
+};
+
 int main()
 {
-    MyClass<std::string> obj1("mahmoud", "skhairi");
-    std::cout << "first: " << obj1.Getfirst() << ", second: " << obj1.Getsecond() << std::endl;
+    MyClass<std::string> obj1("mahmoud");
+    std::cout << "data: " << obj1.Getdata() << std::endl;
     MyClass<float> obj2(15.5f, 20.0f);
     std::cout << "first: " << obj2.Getfirst() << ", second: " << obj2.Getsecond() << std::endl;
 }
