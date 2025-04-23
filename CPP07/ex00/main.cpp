cd30aa46d9fbda82736fd8160046600346c1960b
mahmoudskhairi <mahmoud.skhairi@gmail.com>
2025-04-23 12:27:31 +0100

    first exercice was finished

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -1,18 +1,39 @@
 #include "whatever.hpp"
-
-template <typename T>
-T max(T a, T b)
-{
-    return (a > b ? a : b);
-}
-template <typename T>
-T min(T a, T b)
-{
-    return (a < b ? a : b);
-}
-int main()
-{
-    std::cout << "max: " << max(2, 5) << std::endl;
-    std::cout << "min: " << min(2, 5) << std::endl;
-    std::cout << "" << std::endl;
-}
+int main( void ) {
+    int a = 2;
+    int b = 3;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    return 0;
+    }
+    
+    
+// int main()
+// {
+//     int a = 2;
+//     int b = 5;
+//     std::cout << "a = " << a << ", b = " << b << std::endl;
+//     swap(a, b);
+//     std::cout << "a = " << a << ", b = " << b << std::endl;
+//     // std::string s1 = "mahmoud";
+//     // std::string s2 = "maamoun";
+//     // std::cout << "#test before swaping#" << std::endl;
+//     // std::cout << "s1: " << s1 << std::endl;
+//     // std::cout << "s2: " << s2 << std::endl;
+//     // swap(s1, s2);
+//     // std::cout << "#test after swaping#" << std::endl;
+//     // std::cout << "s1: " << s1 << std::endl;
+//     // std::cout << "s2: " << s2 << std::endl;
+//     std::cout << "#max abd min tests#" << std::endl;
+//     std::cout << "max: " << max(a, b) << std::endl;
+//     std::cout << "min: " << min(a, b) << std::endl;
+//     std::cout << "" << std::endl;
+// }
